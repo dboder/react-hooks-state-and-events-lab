f29af17dcd7b43eef973e25edc1334e92e073c59
Dorsey B <dorseybec@gmail.com>
2023-04-30 15:52:45 -0700

    lots learned compelte

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+const defaultArr = {
+  liClass: "",
+  buttonText: "Add to Cart",
+  buttonClass: "add",
+  id: 0
+};
+
+const changedArr = {
+  liClass: "in-cart",
+  buttonText: "Remove From Cart",
+  buttonClass: "remove",
+  id:1
+};
 
 function Item({ name, category }) {
+  const [itemConfig, setItemConfig] = useState(defaultArr);
+
+  function toggleCart(e) {
+    console.log(itemConfig);
+    setItemConfig(itemConfig.id === 0 ? {...changedArr} : {...defaultArr});
+  }
   return (
-    <li className="">
+    <li className={itemConfig.liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={itemConfig.buttonClass} onClick={toggleCart}>{itemConfig.buttonText}</button>
     </li>
   );
 }
